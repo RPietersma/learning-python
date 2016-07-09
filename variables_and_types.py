133ab5363e021dd5c214b9e62167da5cb722e0ec
Rachel Pietersma <rachel.pietersma@gmail.com>
2016-07-08 22:50:21 -0400

    Completed numbers and strings assignment

---
+++ variables_and_types.py
@@ -0,0 +1,22 @@
+# Numbers
+
+intg = 10
+print intg
+
+flt = 12.6
+flt = float(17)
+print flt
+
+print flt + intg
+
+
+# Strings
+
+string = "Rachel"
+print string
+
+awesome = "is awesome!"
+print string + ' ' + awesome
+
+big, small = "elephant", "rat"
+print "the " + big + " is bigger than the " + small
