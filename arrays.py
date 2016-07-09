784845d20a7384f4b9d6157c7307ad885ba94763
Rachel Pietersma <rachel.pietersma@gmail.com>
2016-07-09 11:17:04 -0400

    learned about arrays

---
+++ arrays.py
@@ -0,0 +1,11 @@
+students = []
+print students
+
+students = ["Rachel", "Antoine"]
+print students
+
+print students[0]
+print students[1]
+
+print students[0] + " and " + students[1] + " are awesome."
+
