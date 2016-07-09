c5752ee4efaa150ed8ee06d6f24d3425e0380c1f
Rachel Pietersma <rachel.pietersma@gmail.com>
2016-07-09 14:24:38 -0400

    learned string formatting

---
+++ string_formatting.py
@@ -0,0 +1,18 @@
+cat = "Andromeda"
+human = "Rachel"
+print "Hello, %s and %s!" % (cat, human)
+
+age = 2
+# Andromeda is 2 years old.
+print "%s is %d years old." % (cat, age)
+
+# Andromeda is 21 years younger than Rachel
+print "%s is %d years younger than %s" % (cat, age + 19, human)
+
+numb = 11
+print "%x" % (numb)
+
+# Hello Andromeda. Your current balance is 78.243 cuddles.
+balance = 78.243
+print "Hello %s. Your current balance is %.3f cuddles." % (cat, balance)
+
